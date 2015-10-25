4580ddf28fcdec5cdb35b677ec9b1979035c2707
ft <ft@noemail>
2015-10-25 13:10:14 +0100

    added missing ExtensionMethods.cs

---
+++ SilverSim/Scripting.LSL/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SilverSim.Scripting.LSL
+{
+    public static class ExtensionMethods
+    {
+        public static int ToLSLBoolean(this bool v)
+        {
+            return v ? 1 : 0;
+        }
+    }
+}
