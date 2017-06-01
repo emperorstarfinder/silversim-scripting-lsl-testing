8a27ebaeba733ad9fac6bd05db1c874c1a176ad9
ft <ft@noemail>
2017-06-01 21:11:35 +0200

    added llSitOnLink stub

--- SilverSim/Scripting.Lsl/Api/Experience/ExperienceApi.cs
+++ SilverSim/Scripting.Lsl/Api/Experience/ExperienceApi.cs
@@ -72,6 +72,21 @@ namespace SilverSim.Scripting.Lsl.Api.Experience
         [APILevel(APIFlags.LSL)]
         public const int XP_ERROR_MATURITY_EXCEEDED = 16;
 
+        [APILevel(APIFlags.LSL)]
+        public const int SIT_NOT_EXPERIENCE = -1;
+        [APILevel(APIFlags.LSL)]
+        public const int SIT_NO_EXPERIENCE_PERMISSIONS = -2;
+        [APILevel(APIFlags.LSL)]
+        public const int SIT_NO_SIT_TARGET = -3;
+        [APILevel(APIFlags.LSL)]
+        public const int SIT_INVALID_AGENT = -4;
+        [APILevel(APIFlags.LSL)]
+        public const int SIT_INVALID_LINK = -5;
+        [APILevel(APIFlags.LSL)]
+        public const int SIT_NO_ACCESS = -6;
+        [APILevel(APIFlags.LSL)]
+        public const int SIT_INVALID_OBJECT = -7;
+
         [APILevel(APIFlags.LSL, "experience_permissions")]
         [StateEventDelegate]
         public delegate void State_experience_permissions(LSLKey agent_id);
@@ -164,6 +179,23 @@ namespace SilverSim.Scripting.Lsl.Api.Experience
         public LSLKey UpdateKeyValue(ScriptInstance instance, string k, string v, int checked_orig, string original_value)
         {
             throw new NotImplementedException("llUpdateKeyValue(string, string, integer, string)");
+        }
+
+        [APILevel(APIFlags.LSL, "llSitOnLink")]
+        public int SitOnLink(ScriptInstance instance, LSLKey agent_id, int link)
+        {
+            /*
+             * The avatar specified by agent_id is forced to sit on the sit target of the prim indicated by the link parameter. If the specified link is already occupied, the simulator searches down the chain of prims in the link set looking for an available sit target.
+             * 
+             * If successful, this method returns 1.
+             * If the function fails, it returns a negative number constant.
+             * Link constants that indicate a single prim may be used for the link parameter. These are LINK_ROOT and LINK_THIS. Other constants such as LINK_SET, LINK_CHILDREN, LINK_ALL_OTHERS will return an INVALID_LINK error.
+             * This method must be called from an experience enabled script running on land that has enabled the experience key. If these conditions are not met this method returns a NOT_EXPERIENCE error.
+             * The targeted avatar must also have accepted the experience. If the user is not participating in the experience this method returns NO_EXPERIENCE_PERMISSION. If the avatar id can not be found or is not over land that has enabled the experience this method returns INVALID_AGENT.
+             * If there are no valid sit targets remaining in the linkset this method returns NO_SIT_TARGET and no action is taken with the avatar.
+             * If the avatar does not have access to the parcel containing the prim running this script, this call fails. 
+             */
+            throw new NotImplementedException("llSitOnLink(key, int)");
         }
     }
 }
