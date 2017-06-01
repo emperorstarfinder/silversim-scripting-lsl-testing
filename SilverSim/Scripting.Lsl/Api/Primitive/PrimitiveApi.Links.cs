f266d1ccf8e4e06dfe237d582c5b41cf6be87ffe
ft <ft@noemail>
2017-06-01 21:02:34 +0200

    added link functions

--- SilverSim/Scripting.Lsl/Api/Primitive/PrimitiveApi.Links.cs
+++ SilverSim/Scripting.Lsl/Api/Primitive/PrimitiveApi.Links.cs
@@ -21,47 +21,191 @@
 
 #pragma warning disable IDE0018, RCS1029
 
+using SilverSim.Scene.Types.Object;
+using SilverSim.Scene.Types.Scene;
 using SilverSim.Scene.Types.Script;
+using SilverSim.Types;
+using SilverSim.Types.Script;
 using System;
+using System.Collections.Generic;
 
 namespace SilverSim.Scripting.Lsl.Api.Primitive
 {
     public partial class PrimitiveApi
     {
+        private void CheckDeLinkingPerms(ScriptInstance instance, Action delinkAction)
+        {
+            ObjectGroup grp = instance.Part.ObjectGroup;
+            ObjectPartInventoryItem.PermsGranterInfo grantinfo = instance.Item.PermsGranter;
+            if (grp.IsAttached)
+            {
+                return;
+            }
+            else if(!instance.Part.CheckPermissions(instance.Part.Owner, instance.Part.Group, Types.Inventory.InventoryPermissionsMask.Modify))
+            {
+                instance.ShoutError(new LocalizedScriptMessage(this, "DelinkFailedDueNoEditPerms", "Delink failed because you do not have edit permission"));
+            }
+            else if (grantinfo.PermsGranter != UUI.Unknown && (grantinfo.PermsMask & ScriptPermissions.ChangeLinks) != 0)
+            {
+                if (grantinfo.PermsGranter != instance.Part.Owner)
+                {
+                    instance.ShoutError(new LocalizedScriptMessage(this, "OnlyOwnerIsAbleToDelinkObjectByScript", "Only owner is able to delink by script"));
+                }
+                else
+                {
+                    delinkAction();
+                }
+            }
+        }
+
+        private void CheckLinkingPerms(ScriptInstance instance, Action linkAction)
+        {
+            ObjectGroup grp = instance.Part.ObjectGroup;
+            ObjectPartInventoryItem.PermsGranterInfo grantinfo = instance.Item.PermsGranter;
+            if (grp.IsAttached)
+            {
+                return;
+            }
+            else if (!instance.Part.CheckPermissions(instance.Part.Owner, instance.Part.Group, Types.Inventory.InventoryPermissionsMask.Modify))
+            {
+                instance.ShoutError(new LocalizedScriptMessage(this, "LinkFailedDueNoEditPerms", "Linking failed because you do not have edit permission"));
+            }
+            else if (grantinfo.PermsGranter != UUI.Unknown && (grantinfo.PermsMask & ScriptPermissions.ChangeLinks) != 0)
+            {
+                if (grantinfo.PermsGranter != instance.Part.Owner)
+                {
+                    instance.ShoutError(new LocalizedScriptMessage(this, "OnlyOwnerIsAbleToLinkObjectByScript", "Only owner is able to link by script"));
+                }
+                else
+                {
+                    linkAction();
+                }
+            }
+        }
+
         [APILevel(APIFlags.LSL, "llCreateLink")]
+        [ForcedSleep(1.0)]
         public void CreateLink(ScriptInstance instance, LSLKey key, int parent)
         {
+            lock (instance)
+            {
+                CheckLinkingPerms(instance, () =>
+                {
+                    ObjectGroup thisGrp = instance.Part.ObjectGroup;
+                    SceneInterface scene = thisGrp.Scene;
+                    ObjectGroup targetGrp;
+                    if (!scene.ObjectGroups.TryGetValue(key, out targetGrp))
+                    {
+                        return;
+                    }
+
+                    if (!targetGrp.Owner.EqualsGrid(thisGrp.Owner))
+                    {
+                        return;
+                    }
+
+                    if (parent != 0)
+                    {
+                        scene.LinkObjects(new List<UUID> { thisGrp.ID, targetGrp.ID });
+                    }
+                    else
+                    {
+                        scene.LinkObjects(new List<UUID> { targetGrp.ID, thisGrp.ID });
+                    }
+                });
+            }
+
             throw new NotImplementedException("llCreateLink(key, integer)");
         }
 
         [APILevel(APIFlags.LSL, "llBreakLink")]
         public void BreakLink(ScriptInstance instance, int link)
         {
-            throw new NotImplementedException("llBreakLink(integer)");
+            lock (instance)
+            {
+                CheckDeLinkingPerms(instance, () =>
+                {
+                    ObjectGroup grp = instance.Part.ObjectGroup;
+                    grp.AgentSitting.UnSitAll();
+                    ObjectPart part;
+                    if (grp.TryGetValue(link, out part))
+                    {
+                        grp.Scene.UnlinkObjects(new List<UUID> { part.ID });
+                    }
+                });
+            }
         }
 
         [APILevel(APIFlags.LSL, "llBreakAllLinks")]
         public void BreakAllLinks(ScriptInstance instance)
         {
-            throw new NotImplementedException("llBreakAllLinks()");
+            lock (instance)
+            {
+                CheckDeLinkingPerms(instance, () =>
+                {
+                    ObjectGroup grp = instance.Part.ObjectGroup;
+                    grp.AgentSitting.UnSitAll();
+                    grp.Scene.UnlinkObjects(grp.Keys2);
+                });
+            }
         }
 
         [APILevel(APIFlags.OSSL, "osForceCreateLink")]
+        [ThreatLevelRequired(ThreatLevel.VeryLow, "osForceCreateLink")]
         public void ForceCreateLink(ScriptInstance instance, LSLKey target, int parent)
         {
-            throw new NotImplementedException("osForceCreateLink(key, integer)");
+            lock (instance)
+            {
+                ObjectGroup thisGrp = instance.Part.ObjectGroup;
+                SceneInterface scene = thisGrp.Scene;
+                ObjectGroup targetGrp;
+                if (!scene.ObjectGroups.TryGetValue(target, out targetGrp))
+                {
+                    return;
+                }
+
+                if (!targetGrp.Owner.EqualsGrid(thisGrp.Owner))
+                {
+                    return;
+                }
+
+                if (parent != 0)
+                {
+                    scene.LinkObjects(new List<UUID> { thisGrp.ID, targetGrp.ID });
+                }
+                else
+                {
+                    scene.LinkObjects(new List<UUID> { targetGrp.ID, thisGrp.ID });
+                }
+            }
         }
 
         [APILevel(APIFlags.OSSL, "osForceBreakLink")]
+        [ThreatLevelRequired(ThreatLevel.VeryLow, "osForceCreateLink")]
         public void ForceBreakLink(ScriptInstance instance, int link)
         {
-            throw new NotImplementedException("osForceBreakLink(integer)");
+            lock(instance)
+            {
+                ObjectGroup grp = instance.Part.ObjectGroup;
+                grp.AgentSitting.UnSitAll();
+                ObjectPart part;
+                if (grp.TryGetValue(link, out part))
+                {
+                    grp.Scene.UnlinkObjects(new List<UUID> { part.ID });
+                }
+            }
         }
 
         [APILevel(APIFlags.OSSL, "osForceBreakAllLinks")]
+        [ThreatLevelRequired(ThreatLevel.VeryLow, "osForceCreateLink")]
         public void ForceBreakAllLinks(ScriptInstance instance)
         {
-            throw new NotImplementedException("osForceBreakAllLinks()");
+            lock(instance)
+            {
+                ObjectGroup grp = instance.Part.ObjectGroup;
+                grp.AgentSitting.UnSitAll();
+                grp.Scene.UnlinkObjects(grp.Keys2);
+            }
         }
     }
 }
