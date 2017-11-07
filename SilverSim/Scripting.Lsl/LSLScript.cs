6e42cedbc7058359901d7fec523ad20b6736347a
ft <ft@noemail>
2017-11-07 19:28:52 +0100

    code cleanup

--- SilverSim/Scripting.Lsl/LSLScript.cs
+++ SilverSim/Scripting.Lsl/LSLScript.cs
@@ -38,7 +38,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Reflection;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading;
 using System.Timers;
