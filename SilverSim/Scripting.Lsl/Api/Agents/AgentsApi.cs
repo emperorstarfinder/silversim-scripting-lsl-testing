f43ffde389275c9f343e5a5fee7d32fd40eda1c8
ft <ft@noemail>
2017-12-16 12:10:59 +0100

    add AGENT_AWAY handling

--- SilverSim/Scripting.Lsl/Api/Agents/AgentsApi.cs
+++ SilverSim/Scripting.Lsl/Api/Agents/AgentsApi.cs
@@ -174,7 +174,12 @@ namespace SilverSim.Scripting.Lsl.Api.Base
                         flags |= AGENT_FLYING;
                     }
 
-                    /* TODO: AGENT_AWAY, AGENT_BUSY, AGENT_CROUCHING, AGENT_IN_AIR, AGENT_TYPING, AGENT_WALKING */
+                    if(agent.IsAway)
+                    {
+                        flags |= AGENT_AWAY;
+                    }
+
+                    /* TODO: AGENT_BUSY, AGENT_CROUCHING, AGENT_IN_AIR, AGENT_TYPING, AGENT_WALKING */
 
                     if(agent.SittingOnObject != null)
                     {
