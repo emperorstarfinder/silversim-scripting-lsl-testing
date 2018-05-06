1f3d66017160edd87aedeb13c8fce310a239a848
ft <ft@noemail>
2018-05-06 10:02:37 +0200

    add specific check regarding being both sides key

--- SilverSim/Scripting.Lsl/LSLCompiler.ILGen.Expressions.BinaryOperators.cs
+++ SilverSim/Scripting.Lsl/LSLCompiler.ILGen.Expressions.BinaryOperators.cs
@@ -1848,7 +1848,7 @@ namespace SilverSim.Scripting.Lsl
                         }
                         else if(m_LeftHandType == typeof(string) || m_LeftHandType == typeof(LSLKey))
                         {
-                            if(!compileState.LanguageExtensions.EnableKeyAndStringConcat && m_LeftHandType != m_RightHandType)
+                            if(!compileState.LanguageExtensions.EnableKeyAndStringConcat && (m_LeftHandType != m_RightHandType || m_LeftHandType == typeof(LSLKey)))
                             {
                                 throw new CompilerException(m_LineNumber, string.Format(this.GetLanguageString(compileState.CurrentCulture, "OperatorPlusNotSupportedFor0And1", "operator '+' is not supported for '{0}' and '{1}'"), compileState.MapType(m_LeftHandType), compileState.MapType(m_RightHandType)));
                             }
