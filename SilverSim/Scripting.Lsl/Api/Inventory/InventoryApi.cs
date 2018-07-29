1e0205c9bdb38eb5f9cdba760fca009daed4c4bf
ft <ft@noemail>
2018-07-29 11:53:57 +0200

    missed the local part

--- SilverSim/Scripting.Lsl/Api/Inventory/InventoryApi.cs
+++ SilverSim/Scripting.Lsl/Api/Inventory/InventoryApi.cs
@@ -599,7 +599,7 @@ namespace SilverSim.Scripting.Lsl.Api.Inventory
                     var e = new DataserverEvent
                     {
                         QueryID = UUID.Random,
-                        Data = (landmark.Location - instance.Part.ObjectGroup.Scene.GetRegionInfo().Location).ToString()
+                        Data = (landmark.Location + landmark.LocalPos - instance.Part.ObjectGroup.Scene.GetRegionInfo().Location).ToString()
                     };
                     instance.PostEvent(e);
                     return e.QueryID;
